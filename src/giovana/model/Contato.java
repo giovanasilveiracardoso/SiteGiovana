42fa12affe93d4c920af238da89370fd8c2ab113
giovana <giovanasilveiracardoso@gmail.com>
2016-01-12 00:03:34 -0200

    criando

---
+++ src/giovana/model/Contato.java
@@ -0,0 +1,41 @@
+package giovana.model;
+
+public class Contato {
+
+	private String nome;
+	private String email;
+	private String assunto;
+	private String descricao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
